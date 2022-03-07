2a6783a1d0fd3c94f776e50f8f841df3205db7b4
Prekshya Dawadi <prekshya019@gmail.com>
2022-03-07 20:12:01 +0545

    Dashboard and Login Sign up integration 2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QGridLayout>
 
 int main(int argc, char *argv[])
 {
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -21,6 +21,8 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_Login_clicked()
 {
+
+
     QString VoterID =ui->lineEdit_Name->text();
     QString password =ui->lineEdit_2_Password->text();
 
@@ -58,21 +60,28 @@ void MainWindow::dashboard()
 
     // Creating push buttons
     QPushButton *BallotButton = new QPushButton("Ballot");
-    QPushButton *PartyButton = new QPushButton("Parties Information");
+    //QPushButton *PartyButton = new QPushButton("Parties Information");
     QPushButton *CandidatesButton = new QPushButton("Candidates Information");
     QPushButton *ResultButton = new QPushButton("Results");
     QPushButton *HelpButton = new QPushButton("Help");
 
     // Adding push buttons to layout
     layout2->addWidget(BallotButton, 0, 0);
-    layout2->addWidget(PartyButton, 0, 1);
+    //addWidget(BallotButton, 0, 0);
+    //layout2->addWidget(PartyButton, 0, 1);
+    //addWidget(PartyButton, 0, 1);
     layout2->addWidget(CandidatesButton, 0, 2);
+    //addWidget(CandidatesButton, 0, 2);
     layout2->addWidget(ResultButton, 1, 1);
+    //addWidget(ResultButton, 1, 1);
     layout2->addWidget(HelpButton, 1, 2);
+    //addWidget(HelpButton, 1, 2);
 
     window->setLayout(layout2);
+    setLayout(layout2);
 
     window->show();
+    show();
 }
 
 
